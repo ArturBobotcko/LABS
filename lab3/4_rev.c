845fe2c570abc8b9f1311b80a65a89b6910199a7
ArturBobotcko <artur.bobotcko@mail.ru>
2022-03-25 22:21:36 +0300

    Did 4 task and modified Makefile

--- lab3/4_rev.c
+++ lab3/4_rev.c
@@ -1,9 +1,38 @@
 #include "../main.h"
+#define SIZE 50000
 
-int main() {
-	
-	
-	
-	
+int main(int argc, char* argv[]) {
+	char buf[SIZE] = {'\0'};
+	int k = 0;
+	for (int i = 1; i < argc; i++) {
+		int fd = open(argv[i], O_RDONLY);
+		
+		if (fd < 0) {
+			perror(argv[i]);
+			return 1;
+		}
+
+		int bytes;
+		off_t t = lseek(fd, -1, SEEK_END); // Ставим смещение на последний байт файла
+		while (t >= 0) {
+			bytes = read(fd, &buf[k], 1); // Считываем по 1 байту (1 символу)
+			if (bytes == -1) {
+				perror(argv[i]);
+				return 2;
+			}
+			t = lseek(fd, -2, SEEK_CUR); // Смещаемся от конца к началу по 1 байту
+			k++;
+		}
+		
+		if (close(fd) == -1) {
+			perror(argv[i]);
+			return 3;
+		}
+	}
+
+	for (int i = 0; i < SIZE; i++) { // Вывод буфера
+			printf("%c",buf[i]);
+	}
+
 	return 0;
 }
