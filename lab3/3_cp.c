d0fbc3e7886170038a1d517077a51a1ac6650f20
ArturBobotcko <artur.bobotcko@mail.ru>
2022-03-26 18:39:28 +0300

    Fixed and modified task №3

--- lab3/3_cp.c
+++ lab3/3_cp.c
@@ -1,10 +1,8 @@
 #include "../main.h"
-#define SIZE 50000
 
 /* argv[1] - первый файл */
 /* argv[2] - второй файл */
 int main(int argc, char* argv[]) {
-	char buffer[SIZE];
 	if (argc == 1) {
 		printf("Не указаны файлы\n");
 		return 1;
@@ -21,31 +19,43 @@ int main(int argc, char* argv[]) {
 		return 3;
 	}
 
-	int FILE_2 = open(argv[2], O_WRONLY); // Открываем второй файл на запись
-	if (FILE_2 < 0) {
-		perror(argv[2]);
-		return 4;
+	off_t SIZE = lseek(FILE_1, 0, SEEK_END); // Определяем размер записанных данных первого файла
+	if (SIZE == -1) {
+		perror(argv[1]);
+		return 4
 	}
-	
-	int bytes = read(FILE_1, &buffer[0], SIZE); // Считываем данные из первого файла
-	if (bytes == -1) {
+
+	char buffer[SIZE];
+	if (lseek(FILE_1, 0, SEEK_SET) == -1) { // Возвращаем смещение в начало
 		perror(argv[1]);
 		return 5;
 	}
 
-	if (write(FILE_2, &buffer[0], bytes) == -1) { // Записываем считанные данные во второй файл
+	int FILE_2 = open(argv[2], O_WRONLY | O_TRUNC); // Открываем второй файл на запись
+	if (FILE_2 < 0) {
 		perror(argv[2]);
 		return 6;
 	}
-
-	if (close(FILE_1) == -1) {
+	
+	ssize_t bytes = read(FILE_1, &buffer[0], SIZE); // Считываем данные из первого файла
+	if (bytes == -1) {
 		perror(argv[1]);
 		return 7;
 	}
 
-	if (close(FILE_2) == -1) {
+	if (write(FILE_2, &buffer[0], bytes) == -1) { // Записываем считанные данные во второй файл
 		perror(argv[2]);
 		return 8;
+	}
+
+	if (close(FILE_1) == -1) {
+		perror(argv[1]);
+		return 9;
+	}
+
+	if (close(FILE_2) == -1) {
+		perror(argv[2]);
+		return 10;
 	}
 
 	return 0;
