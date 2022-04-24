0d513a92ce7b888b2d3e5d8bfc7d6399533a5836
ArturBobotcko <artur.bobotcko@mail.ru>
2022-04-24 21:56:43 +0300

    Did №3 for lab6

--- lab6/3_thread_attr.c
+++ lab6/3_thread_attr.c
@@ -1,8 +1,43 @@
 #include "../main.h"
 
-int main(int argc, char* argv[]) {
-	
+void* command(void* p) {
+	printf("Hello from command thread\n");
+	pthread_exit((void *)100);
+}
 
+int main(int argc, char* argv[]) {
+	int retval = 0, err;
+	pthread_t thread;
+	pthread_attr_t attr;
 	
+	err = pthread_attr_init(&attr);
+	if (err != 0) {
+		perror("pthread_attr_init");
+		return -1;
+	}
+	err = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+	int pt = pthread_create(
+		&thread,
+		&attr,
+		command,
+		NULL
+	);
+	pthread_attr_destroy(&attr);
+	pthread_join(thread, (void*)&retval);
+
+	printf("retval = %d\n\n", retval);
+
+	pt = pthread_create(
+		&thread,
+		NULL,
+		command,
+		NULL
+	);
+
+	pthread_join(thread, (void*)&retval);
+
+	printf("retval = %d\n", retval);
+
 	return 0;
 }
